6b6c2f003eaf8a7196878b52db6064b2588eb0a1
Mohamed Hossam <m.hossameldine@gmail.com>
2022-04-04 19:31:16 +0200

    feat: change password API

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,46 @@
+import { useRef, useContext } from 'react';
 import classes from './ProfileForm.module.css';
 
+import { API_KEY } from '../../constants/api';
+import AuthContext from '../../store/auth-context';
+
 const ProfileForm = () => {
+  const passwordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const newEnteredPassword = passwordInputRef.current.value;
+
+    // could add validation
+
+    let URL = `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${API_KEY}`;
+    fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: newEnteredPassword,
+        returnSecureToken: false,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => {
+      // could add error handling, but for now I will assume that the request always success
+    });
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' ref={passwordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
